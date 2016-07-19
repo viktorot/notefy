21e1138ea65c08f079e415c15cebb34a024f9e91
Viktor Chuchurski <viktor.chuchurski@gmail.com>
2016-07-19 21:08:33 +0200

    - animation test

--- app/src/main/kotlin/org/viktorot/notefy/view/MainActivity.kt
+++ app/src/main/kotlin/org/viktorot/notefy/view/MainActivity.kt
@@ -1,18 +1,20 @@
 package org.viktorot.notefy.view
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.graphics.Point
 import android.graphics.Rect
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v4.view.animation.FastOutLinearInInterpolator
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewAnimationUtils
+import android.view.*
+import android.view.animation.Animation
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
@@ -36,15 +38,54 @@ class MainActivity : AppCompatActivity() {
 //
 //        iconRecyclerView.itemAnimator = animator
 
-        var btn = findViewById(R.id.btn) as ImageButton
-        btn.setOnClickListener { view ->
-            ViewAnimationUtils.createCircularReveal(view, view.width, view.height, 0f, view.height * 2f).start()
+        var selected: Boolean = true
+        val target = findViewById(R.id.vju)
+        val targetSelected = findViewById(R.id.vjuSelected)
+
+        val gestureListener = object: GestureDetector.SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent?): Boolean {
+                return true
+            }
+
+            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                val x = if(e == null) 0 else e.x.toInt()
+                val y = if(e == null) 0 else e.y.toInt()
+
+                target.visibility = View.VISIBLE
+
+                val animator: Animator
+                if(selected) {
+                    animator = getHideAnimator(target, x, y)
+                }
+                else {
+                    animator = getRevealAnimator(target, x, y)
+                }
+
+                selected = !selected;
+                animator.start()
+
+                return true
+            }
         }
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Toast.makeText(this.applicationContext, "Kwic", Toast.LENGTH_SHORT).show() }
-    }
+        var gestureDetector = GestureDetector(this, gestureListener)
 
+
+        target.setOnTouchListener(object: View.OnTouchListener {
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                return gestureDetector.onTouchEvent(p1)
+            }
+        })
+
+        targetSelected.setOnTouchListener(object: View.OnTouchListener {
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                return gestureDetector.onTouchEvent(p1)
+            }
+        })
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view -> }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -68,7 +109,80 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    private fun animateElementSelect(targetView: View) {
+        val cx = targetView.width / 2
+        val cy = targetView.height / 2
+
+        val radius: Float = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+        val animator = ViewAnimationUtils.createCircularReveal(targetView, cx, cy, 0f, radius)
+
+        targetView.visibility = View.VISIBLE
+        animator.start()
+    }
+
+    private fun animateElementDeselect(targetView: View) {
+        val cx = targetView.width / 2
+        val cy = targetView.height / 2
+
+        val radius: Float = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+        val animator = ViewAnimationUtils.createCircularReveal(targetView, cx, cy, radius, 0f)
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                targetView.visibility = View.INVISIBLE
+            }
+        })
+
+        animator.start()
+    }
+
+    private fun getRevealAnimator(target: View, touchX: Int, touchY: Int): Animator {
+        val cx = target.width / 2
+        val cy = target.height / 2
+
+        val p1 = Point(touchX, touchY)
+        val p2 = Point(cx, cy)
+
+        val radius0 = Math.sqrt(Math.pow((p1.y - p2.y).toDouble(), 2.0) + Math.pow((p1.x - p2.x).toDouble(), 2.0)).toFloat()
+
+        val radius: Float = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat() + radius0
+
+        val animator = ViewAnimationUtils.createCircularReveal(target, touchX, touchY, 0f, radius)
+        animator.interpolator = FastOutLinearInInterpolator()
+
+        return animator
+    }
+
+    private fun getHideAnimator(target: View, touchX: Int, touchY: Int): Animator {
+        val cx = target.width / 2
+        val cy = target.height / 2
+
+        val p1 = Point(touchX, touchY)
+        val p2 = Point(cx, cy)
+
+        val radius0 = Math.sqrt(Math.pow((p1.y - p2.y).toDouble(), 2.0) + Math.pow((p1.x - p2.x).toDouble(), 2.0)).toFloat()
+
+        val radius: Float = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat() + radius0
+
+        val animator = ViewAnimationUtils.createCircularReveal(target, touchX, touchY, radius, 0f)
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                target.visibility = View.INVISIBLE
+            }
+        })
+        animator.interpolator = FastOutLinearInInterpolator()
+
+        return animator
+    }
+
+
+
 }
+
 
 private class MarginItemDecoration(size: Int) : RecyclerView.ItemDecoration() {
     private val margin: Int
